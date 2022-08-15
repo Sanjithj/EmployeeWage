b5207a5aad69afff67edbd6e28fc9e178e2f842c
Sanjithj <sanjithj591997@gmail.com>
2022-08-15 19:49:22 +0530

    Added: Employee present absent

--- WageComputation.java
+++ WageComputation.java
@@ -4,6 +4,15 @@ public class WageComputation {
 	public static void main(String[] args) {
 		
 		System.out.println("Welcome to Employee wage computation program");
+		
+		 double empWage = (int) Math.floor(Math.random() * 10) % 2;   
+	        if (empWage == 1)
+	        {
+	            System.out.println("Employee is Present");
+	        } 
+	        else {
+	            System.out.println("Employee is Absent");
+	        }
 	}
 
 }
