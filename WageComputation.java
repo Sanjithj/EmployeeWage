70a4333418a3610c59e15b377fd2fdf960633cf9
Sanjithj <sanjithj591997@gmail.com>
2022-08-15 19:27:22 +0530

    Added: Welcome to employee wage

---
+++ WageComputation.java
@@ -0,0 +1,9 @@
+package com.bridgelabz;
+
+public class WageComputation {
+	public static void main(String[] args) {
+		
+		System.out.println("Welcome to Employee wage computation program");
+	}
+
+}
