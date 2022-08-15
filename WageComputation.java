3e322b4e83659d610345cd7033d371b9c00858f0
Sanjithj <sanjithj591997@gmail.com>
2022-08-15 20:36:48 +0530

    Added: Switch

--- WageComputation.java
+++ WageComputation.java
@@ -1,23 +1,28 @@
 package com.bridgelabz;
 
 public class WageComputation {
+ 	public static final int IS_PART_TIME = 1;
+   	public static final int IS_FULL_TIME = 2;
+   	public static final int EMP_RATE_PER_HRS= 20;
+   	
 	public static void main(String[] args) {
 		
 		System.out.println("Welcome to Employee wage computation program");
 		
-		int IS_PART_TIME = 1;
-		int IS_FULL_TIME = 2;
-		int EMP_RATE_PER_HRS= 20;
 		int empHrs = 0;
 		int empRate = 0;
 		
-		 double empWage = (int) Math.floor(Math.random() * 10) % 2;   
-	        if (empWage == IS_PART_TIME)
-	        	empHrs = 4;
-	        else if (empWage == IS_FULL_TIME)
+		int empWage = (int) Math.floor(Math.random() * 10) % 2;   
+	       switch (empWage) {
+	       case IS_PART_TIME:
+	    	   empHrs = 4;
+	       break;
+	       case IS_FULL_TIME:
 	        	empHrs = 8;
-	        else
+	       break;
+	       default:
 	        	empHrs = 0;
+	       }
 	        empRate = empHrs * EMP_RATE_PER_HRS;
 	        System.out.println("Emp wage " + empRate);
 	}
